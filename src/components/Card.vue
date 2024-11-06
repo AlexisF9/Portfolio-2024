e5dc78e02f9d67bd9c01205e77cc0a490ee7966b
Alexis <alexis.flacher@mezcalito.fr>
2024-11-06 17:12:03 +0100

    edit responsive

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="c-card">
+    <div>
+      <div class="c-card__title">
+        <h3 class="c-h-l">{{ title }}</h3>
+      </div>
+      <img :src="picture" alt=""/>
+      <div class="c-card__footer">
+        <img class="c-card__techno" :src="`/images/${getTechno(techno)}.png`" :alt="`Logo ${techno}`" />
+        <p class="c-card__type c-text-s">{{ type === "perso" ? "Projet perso" : "Projet pro" }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string,
+  type: string,
+  techno: string,
+  picture: string
+}
+defineProps<Props>()
+
+const getTechno = (element: string) => {
+  return "logo-" + element
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,16 +7,20 @@
           <Transition name="home-test" appear>
             <div class="c-home__swiper">
               <Swiper
-                  :slides-per-view="'auto'"
-                  :space-between="40"
-                  :direction="'vertical'"
+                  :breakpoints="{
+                    '320': {
+                      slidesPerView: 5,
+                      direction: 'horizontal'
+                    },
+                    '1024': {
+                      slidesPerView: 'auto',
+                      direction: 'vertical',
+                      spaceBetween: 40
+                    }
+                  }"
               >
                 <SwiperSlide v-for="item in data" :key="item.id">
-                  <div class="c-home__card">
-                    <div>
-                      {{ item.title }}
-                    </div>
-                  </div>
+                  <Card :title="item.title" :type="item.type" :techno="item.techno" :picture="item.picture"/>
                 </SwiperSlide>
               </Swiper>
             </div>
@@ -33,5 +37,21 @@ import {Swiper, SwiperSlide} from "swiper/vue";
 import rea from '../../public/realisations.json';
 
 import {ref} from "vue";
+import Card from "../components/Card.vue";
 const data = ref(rea)
 </script>
+
+<!--
+:breakpoints="{
+                    '320': {
+                      touchRatio: 0,
+                      shortSwipes: false,
+                      longSwipes: false
+                    },
+                    '1024': {
+                      touchRatio: 1,
+                      shortSwipes: true,
+                      longSwipes: true
+                    }
+                  }"
+-->
