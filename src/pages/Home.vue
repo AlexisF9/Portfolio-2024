ee02e0ecb71ecae918a60dcc47481002c9fd9b63
Alexis <alexis.flacher38@gmail.com>
2024-11-07 17:37:08 +0100

    add new footer link + edit single rea page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -35,7 +35,8 @@
       </div>
     </div>
     <div class="c-home__footer">
-      <a href="https://github.com/AlexisF9" target="_blank" class="c-text-m u-text-white">Visiter mon GitHub</a>
+      <a href="https://github.com/AlexisF9" target="_blank" class="c-text-m u-text-white">Voir mon GitHub</a>
+      <a href="https://www.linkedin.com/in/alexis-flacher-772ba7197/" target="_blank" class="c-text-m u-text-white">LinkedIn</a>
       <a href="mailto:alexis.flacher38@gmail.com" class="c-text-m u-text-white">Me contacter</a>
     </div>
   </div>
