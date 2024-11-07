83080fdbadefb5dee2f999fc51028bf126550750
Alexis <alexis.flacher38@gmail.com>
2024-11-07 15:37:05 +0100

    edit cards style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="c-card">
-    <div>
-      <div class="c-card__title">
-        <h3 class="c-h-l">{{ title }}</h3>
-        <p class="c-card__type c-text-s">{{ type === "perso" ? "Projet perso" : "Projet pro" }}</p>
+    <div class="c-card__content">
+      <div>
+        <div class="c-card__title">
+          <h3 class="c-h-l">{{ title }}</h3>
+          <p class="c-card__type c-text-s">{{ type === "perso" ? "Projet perso" : "Projet pro" }}</p>
+        </div>
+        <p>{{ desc }}</p>
       </div>
-      <p>{{ desc }}</p>
       <div class="c-card__footer">
         <img v-for="techno in technos" class="c-card__techno" :src="`/images/${getTechno(techno)}.png`" :alt="`Logo ${techno}`" />
       </div>
-      <RouterLink :to="{name: 'single-rea', params: {name: name}}">Voir plus</RouterLink>
+      <RouterLink :to="{name: 'single-rea', params: {name: name}}" class="u-sr-only">Voir plus</RouterLink>
     </div>
   </div>
 </template>
