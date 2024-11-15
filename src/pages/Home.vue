004a7a66afec846f4e66385bc30f2a6d9b53cd1a
AlexisF9 <alexis.flacher38@gmail.com>
2024-11-15 10:24:16 +0100

    add short description and edit projects descriptions

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,32 +2,32 @@
   <div class="c-home">
     <div class="o-container o-container--sm">
       <div class="c-home__content">
-        <Hero/>
+        <Hero />
         <div class="c-home__realisations">
           <Transition name="home-slider" appear>
             <div class="c-home__swiper">
               <Swiper
-                  slides-per-view="auto"
-                  :mousewheel="true"
-                  :modules=[Mousewheel]
-                  :breakpoints="{
-                    '320': {
-                      direction: 'horizontal',
-                      spaceBetween: 20
-                    },
-                    '1024': {
-                      direction: 'vertical',
-                      spaceBetween: 40
-                    }
-                  }"
+                slides-per-view="auto"
+                :mousewheel="true"
+                :modules="[Mousewheel]"
+                :breakpoints="{
+                  '320': {
+                    direction: 'horizontal',
+                    spaceBetween: 20,
+                  },
+                  '1024': {
+                    direction: 'vertical',
+                    spaceBetween: 40,
+                  },
+                }"
               >
                 <SwiperSlide v-for="item in data" :key="item.id">
                   <Card
-                      :name="item.name"
-                      :title="item.title"
-                      :type="item.type"
-                      :technos="item.technos"
-                      :desc="item.description"
+                    :name="item.name"
+                    :title="item.title"
+                    :type="item.type"
+                    :technos="item.technos"
+                    :desc="item.shortDescription"
                   />
                 </SwiperSlide>
               </Swiper>
@@ -37,10 +37,27 @@
       </div>
     </div>
     <div class="c-home__footer">
-      <a href="https://github.com/AlexisF9" target="_blank" class="c-text-m u-text-white">GitHub</a>
-      <a href="https://www.linkedin.com/in/alexis-flacher-772ba7197/" target="_blank" class="c-text-m u-text-white">LinkedIn</a>
-      <a href="mailto:alexis.flacher38@gmail.com" class="c-text-m u-text-white">Me contacter</a>
-      <a href="/alexis-flacher-cv.pdf" target="_blank" class="c-text-m u-text-white">Mon CV</a>
+      <a
+        href="https://github.com/AlexisF9"
+        target="_blank"
+        class="c-text-m u-text-white"
+        >GitHub</a
+      >
+      <a
+        href="https://www.linkedin.com/in/alexis-flacher-772ba7197/"
+        target="_blank"
+        class="c-text-m u-text-white"
+        >LinkedIn</a
+      >
+      <a href="mailto:alexis.flacher38@gmail.com" class="c-text-m u-text-white"
+        >Me contacter</a
+      >
+      <a
+        href="/alexis-flacher-cv.pdf"
+        target="_blank"
+        class="c-text-m u-text-white"
+        >Mon CV</a
+      >
     </div>
   </div>
 </template>
@@ -48,11 +65,11 @@
 <script setup lang="ts">
 import Hero from "../components/Hero.vue";
 import Card from "../components/Card.vue";
-import {Swiper, SwiperSlide} from "swiper/vue";
-import rea from '../../public/realisations.json';
-import 'swiper/swiper-bundle.css';
-import { Mousewheel } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/vue";
+import rea from "../../public/realisations.json";
+import "swiper/swiper-bundle.css";
+import { Mousewheel } from "swiper/modules";
 
-import {ref} from "vue";
-const data = ref(rea)
+import { ref } from "vue";
+const data = ref(rea);
 </script>
