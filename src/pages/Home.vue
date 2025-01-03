41da07c09abfc69628b6a811a11f21691980b30d
AlexisF9 <alexis.flacher38@gmail.com>
2025-01-03 13:40:17 +0100

    add rea filters in home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,37 +3,57 @@
     <div class="o-container o-container--sm">
       <div class="c-home__content">
         <Hero />
-        <div class="c-home__realisations">
-          <Transition name="home-slider" appear>
-            <div class="c-home__swiper">
-              <Swiper
-                slides-per-view="auto"
-                :mousewheel="true"
-                :modules="[Mousewheel]"
-                :breakpoints="{
-                  '320': {
-                    direction: 'horizontal',
-                    spaceBetween: 20,
-                  },
-                  '1024': {
-                    direction: 'vertical',
-                    spaceBetween: 40,
-                  },
-                }"
-              >
-                <SwiperSlide v-for="item in data" :key="item.id">
-                  <Card
-                    :name="item.name"
-                    :title="item.title"
-                    :type="item.type"
-                    :technos="item.technos"
-                    :desc="item.shortDescription"
-                    :date="item.date"
-                  />
-                </SwiperSlide>
-              </Swiper>
-            </div>
-          </Transition>
+        <div class="c-home__realisations-container">
+          <Transition name="realisations-buttons" appear
+            ><div class="c-home__realisations-buttons">
+              <p class="c-h-l u-text-white">Réalisations</p>
+              <div>
+                <button
+                  :class="`${
+                    filter === item.value ? 'c-text-m is-active' : 'c-text-m'
+                  }`"
+                  v-for="item in filters"
+                  @click="filter = item.value"
+                >
+                  {{ item.label }}
+                </button>
+              </div>
+            </div></Transition
+          >
+
+          <div class="c-home__realisations">
+            <Transition name="home-slider" appear>
+              <div class="c-home__swiper">
+                <Swiper
+                  @swiper="captureSwiperInstance"
+                  slides-per-view="auto"
+                  :mousewheel="true"
+                  :modules="[Mousewheel]"
+                  :breakpoints="{
+                    '320': {
+                      direction: 'horizontal',
+                      spaceBetween: 20,
+                    },
+                    '1024': {
+                      direction: 'vertical',
+                      spaceBetween: 40,
+                    },
+                  }"
+                >
+                  <SwiperSlide v-for="item in data" :key="item.id">
+                    <Card
+                      :name="item.name"
+                      :title="item.title"
+                      :type="item.type"
+                      :technos="item.technos"
+                      :desc="item.shortDescription"
+                      :date="item.date"
+                    />
+                  </SwiperSlide>
+                </Swiper>
+              </div>
+            </Transition>
+          </div>
         </div>
       </div>
     </div>
@@ -75,6 +95,39 @@ import rea from "../../public/realisations.json";
 import "swiper/swiper-bundle.css";
 import { Mousewheel } from "swiper/modules";
 
-import { ref } from "vue";
+import { ref, watch } from "vue";
 const data = ref(rea);
+const filter = ref("tous");
+const mySwiper: any = ref(null);
+
+const filters: { label: string; value: string }[] = [
+  {
+    label: "Tous",
+    value: "tous",
+  },
+  {
+    label: "Pro",
+    value: "pro",
+  },
+  {
+    label: "Perso",
+    value: "perso",
+  },
+];
+
+const captureSwiperInstance = (swiper: any) => {
+  mySwiper.value = swiper;
+};
+
+watch(filter, () => {
+  mySwiper.value.slideTo(0);
+  if (rea.length > 0) {
+    data.value =
+      filter.value === "pro"
+        ? rea.filter((el) => el.type === "pro")
+        : filter.value === "perso"
+        ? rea.filter((el) => el.type === "perso")
+        : (data.value = rea);
+  }
+});
 </script>
